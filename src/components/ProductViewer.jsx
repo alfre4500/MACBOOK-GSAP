31ea4e7872fc9c6f48576f21ce5e12c445c94113
Alfredo <alfreagor@gmail.com>
2026-03-05 17:21:05 -0300

    switcher finaL

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -17,7 +17,7 @@ const ProductViewer = () => {
     <section id="product-viewer">
       <h2>Take a closer look.</h2>
       <div className="controls">
-        <p className="info">MacbookPro 16" in {color}</p>
+        
         <div className="flex center gap-5  mt-5">
           <div className="color-control">
             <div
--- src/components/models/Macbook-14.jsx
+++ src/components/models/Macbook-14.jsx
@@ -1,26 +1,26 @@
 
 import { useGLTF, useTexture } from '@react-three/drei'
-import { useMacbookStore } from '../../store/index.js';
+import useMacbookStore from '../../store/index.js';
 import { useEffect } from 'react';
 import {Color} from 'three';  
 import { noChangeParts } from '../../constants';
 
 export default  function Macbook14(props) {
   const {color } = useMacbookStore();
-  const { nodes, materials , scenes } = useGLTF('/models/macbook-14-transformed.glb');
+  const { nodes, materials , scene } = useGLTF('/models/macbook-14-transformed.glb');
 
   const texture = useTexture("/screen.png");
 
 
 useEffect(() => {
-  scenes.traverse((child) =>{
+  scene.traverse((child) =>{
     if(child.isMesh){
       if(!noChangeParts.includes(child.name)){
         child.material.color = new Color(color);
       }
     }
   })  
-},[color, scenes])
+},[color, scene])
 
   return (
     <group {...props} dispose={null}>
