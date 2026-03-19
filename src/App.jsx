13942b4d575c62bf674aaeeada614541b33cd869
Alfredo <alfreagor@gmail.com>
2026-03-18 21:52:15 -0300

    implement footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 import { ScrollTrigger} from "gsap/all";
-import Showcase from "./components/models/three/ShowCase";
+import Showcase from "./components/models/three/Showcase";
 import Performance from "./components/models/three/Performance";
 import Features from "./components/models/three/Features";
 import Highlights from "./components/models/three/Highlights";
@@ -19,12 +19,12 @@ const App  = () => {
    <main>
     <NavBar />
     <Hero/>
-    <ProductViewer/>
-    <Showcase/>
+    <ProductViewer/> 
+     <Showcase/>
     <Performance/>
     <Features/>
-    <Highlights/>
-    <Footer/>
+    <Highlights/> 
+     <Footer/>
    </main>
     )
 }
